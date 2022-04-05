61adf329026a81796350c0c4853e51024d595f9b
cestrixx <cestrix@hotmail.com>
2022-04-05 15:54:24 -0300

    initial code

---
+++ index.ts
@@ -0,0 +1,12 @@
+function fromString(value : string) : string {
+    return "FROM STRING" + " " + value;
+}
+
+function toString() : string {
+    return "TO STRING";
+}
+
+module.exports = {
+    fromString,
+    toString
+}
