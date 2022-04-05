6c8cf604c7724183ff7f0dc38f116d2344c44b45
cestrixx <cestrix@hotmail.com>
2022-04-05 20:12:27 -0300

    add dist folder

---
+++ dist/index.js
@@ -0,0 +1,11 @@
+"use strict";
+function fromString(value) {
+    return "FROM STRING" + " " + value;
+}
+function toString() {
+    return "TO STRING";
+}
+module.exports = {
+    fromString,
+    toString
+};
