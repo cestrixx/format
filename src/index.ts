048478893fd03165c7d2c56b4f6329a34673f999
cestrixx <cestrix@hotmail.com>
2022-04-07 14:28:32 -0300

    add degree to radian functions

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
 
 export enum Units {
-    Rad = 0,
-    Dec,
-    Dm,
-    Dms
+    Radian = 0,
+    Degree,
+    DegreeMinute,
+    DegreeMinuteSecond
 }
 
 enum GeographicsCoordinates {
@@ -19,7 +19,7 @@ enum CardinalDirection {
 }
 
 export class Format {
-    static stringToRadians(value: string, unit: Units = Units.Dec): number {
+    static stringToRadian(value: string, unit: Units = Units.Degree): number {
         let cardinalDirection: CardinalDirection = CardinalDirection.Northeast;
         let geographicsCoordinates: GeographicsCoordinates = GeographicsCoordinates.Latitude;
 
@@ -35,17 +35,17 @@ export class Format {
         else if (value.includes('SW')) { cardinalDirection = CardinalDirection.Southwest }
         else if (value.includes('NW')) { cardinalDirection = CardinalDirection.Northwest }
 
-        if (value.endsWith("RAD")) { unit = Units.Rad; } 
-        else if (value.endsWith("D")) { unit = Units.Dms; } 
+        if (value.endsWith("RAD")) { unit = Units.Radian; } 
+        else if (value.endsWith("D")) { unit = Units.DegreeMinuteSecond; } 
         else { /* Units */ }
 
         let radianValue: number; radianValue = 0;
 
         switch (unit) {
-            case Units.Rad : radianValue = parseFloat(value); break;
-            case Units.Dec : radianValue = this.decimalToRadian(value); break;
-            case Units.Dm  : radianValue = this.degreeMinutesToRadian(value); break;
-            case Units.Dms : radianValue = this.degreeMinutesSecondsToRadian(value); break;
+            case Units.Radian: radianValue = parseFloat(value); break;
+            case Units.Degree: radianValue = this.degreeToRadian(value); break;
+            case Units.DegreeMinute: radianValue = this.degreeMinuteToRadian(value); break;
+            case Units.DegreeMinuteSecond: radianValue = this.degreeMinuteSecondToRadian(value); break;
             default: break;
         }
 
@@ -60,25 +60,50 @@ export class Format {
         return radianValue;
     }
 
-    static decimalToRadian(value: string): number {   
-        return 0;
+    static degreeToRadian(value: string): number {
+        value = value.replace(",", ".");
+        value = value.replace("°", "");
+        const degree: number = parseFloat(value);
+        return degreeToRadian(degree);
     }
-    static degreeMinutesToRadian(value: string): number {        
-        return 0;
+    static degreeMinuteToRadian(value: string): number {
+        const regex  = /^[\+-]?([1-8]?\d)\D+([1-6]?\d\.*\d*)\D*$/
+        const values = regex.exec(value);
+        if (values === null)
+            throw new Error("Dados invalidos!")
+        const degree = parseInt(values[1])/1 + parseFloat(values[2])/60;
+        let   radian = degreeToRadian(degree)
+        if (value.indexOf("-") >= 0) radian *= -1
+        return radian;
     }
-    static degreeMinutesSecondsToRadian(value: string): number {        
-        return 0;
+    static degreeMinuteSecondToRadian(value: string): number {        
+        const regex  = /^[\+-]?([1-8]?\d)\D+([1-5]?\d|60)\D+([1-5]?\d[\.\d]+|60)\D*$/
+        const values = regex.exec(value);
+        if (!values)
+            throw new Error("Dados invalidos!")
+        const degree = parseInt(values[1])/1 + parseFloat(values[2])/60 + parseFloat(values[3])/3600;
+        let   radian = degreeToRadian(degree)
+        if (value.indexOf("-") >= 0) radian *= -1
+        return radian;
     }
-    static radianToDegreeMinutes(value: number): string {
+    static radianToDegreeMinute(value: number): string {
         return "";
     }
-    static radianToDegreeMinutesSeconds(value: number): string {
+    static radianToDegreeMinuteSecond(value: number): string {
         return "";
     }
-    static stringToDegreeMinutes(value: string) {
-        return this.radianToDegreeMinutes(this.stringToRadians(value));
+    static stringToDegreeMinute(value: string) {
+        return this.radianToDegreeMinute(this.stringToRadian(value));
     }
-    static stringToDegreeMinutesSeconds(value: string) {
-        return this.radianToDegreeMinutesSeconds(this.stringToRadians(value));
+    static stringToDegreeMinuteSecond(value: string) {
+        return this.radianToDegreeMinuteSecond(this.stringToRadian(value));
     }
+}
+
+export function degreeToRadian(degrees: number): number {
+	return degrees * Math.PI / 180;
+}
+
+export function radianToDegree(radians: number): number {
+	return radians * 180 / Math.PI;
 }
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,7 +1,51 @@
 import { Format } from "../src/index";
 
 describe("Format", () => {
-  it("Decimal To Radian", () => {
-    expect(Format.decimalToRadian("-52.8898106671")).toEqual(0.923101);
-  });
+    it("Degree To Radian => d.d", () => {
+        expect(Format.degreeToRadian("52.8898106671")).toEqual(0.9231013368973136);
+    });
+
+    it("Degree To Radian => -d,d", () => {
+        expect(Format.degreeToRadian("-52,8898106671")).toEqual(-0.9231013368973136);
+    });
+
+    it("DegreeMinute To Radian => d°d.d's", () => {
+        expect(Format.degreeMinuteToRadian("40°26.767'")).toEqual(0.7059179054790872);
+    });
+
+    it("DegreeMinute To Radian => -d°d.d'", () => {
+        expect(Format.degreeMinuteToRadian("-40°26.767'")).toEqual(-0.7059179054790872);
+    });
+
+    it("DegreeMinute To Radian => d d.d", () => {
+        expect(Format.degreeMinuteToRadian("40 26.767")).toEqual(0.7059179054790872);
+    });
+
+    it("DegreeMinute To Radian => d d", () => {
+        expect(Format.degreeMinuteToRadian("40 26")).toEqual(0.7056947942230405);
+    });
+
+    it("DegreeMinute To Radian => d", () => {
+        expect(() => { Format.degreeMinuteToRadian("40"); }).toThrow('Dados invalidos!');
+    });
+
+    it("DegreeMinuteSecond To Radian => d°d'd.d\"", () => {
+        expect(Format.degreeMinuteSecondToRadian("40°26'45.9996\"")).toEqual(0.7059178065770961);
+    });
+
+    it("DegreeMinuteSecond To Radian => -d°d'd.d\"", () => {
+        expect(Format.degreeMinuteSecondToRadian("-40°26'45.9996\"")).toEqual(-0.7059178065770961);
+    });
+
+    it("DegreeMinuteSecond To Radian => d d d.d", () => {
+        expect(Format.degreeMinuteSecondToRadian("40 26 45.9996")).toEqual(0.7059178065770961);
+    });
+
+    it("DegreeMinuteSecond To Radian => d d d", () => {
+        expect(Format.degreeMinuteSecondToRadian("40 26 45")).toEqual(0.7059129603795398);
+    });
+
+    it("DegreeMinute To Radian => d d d", () => {
+        expect(() => { Format.degreeMinuteSecondToRadian("40 26"); }).toThrow('Dados invalidos!');
+    });
 });
